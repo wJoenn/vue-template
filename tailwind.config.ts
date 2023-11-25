4057c9ba38b54f6bdb83a45564ca93a062ff5fac
Louis Ramos <louisramosdev@gmail.com>
2023-11-25 10:45:08 +0100

    add typescript to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,9 @@
+import type { Config } from "tailwindcss"
+
 export default {
-  content: ["./*.html", "./src/**/*.{js,ts,vue}"],
+  content: [],
+  plugins: [],
   theme: {
     extend: {}
-  },
-  plugins: []
-}
+  }
+} satisfies Config
